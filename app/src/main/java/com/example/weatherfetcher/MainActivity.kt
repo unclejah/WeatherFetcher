1beab66af4d8bf64b76fcd29a656e50b87bb3299
jah <celt8@yandex.ru>
2022-08-24 18:38:25 +0500

    - create weather screen

--- app/src/main/java/com/example/weatherfetcher/MainActivity.kt
+++ app/src/main/java/com/example/weatherfetcher/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.weatherfetcher
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnWeather = findViewById<Button>(R.id.btnToWeather)
+        btnWeather.setOnClickListener {
+            Intent(this, WeatherActivity::class.java).also { startActivity(it) }
+        }
     }
 }
